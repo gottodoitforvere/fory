778c2a316ffe1d377b193329b01f03358f5955c6
gottodoitforvere <38937564+gottodoitforvere@users.noreply.github.com>
2018-05-03 11:02:56 +0300

    Add files via upload

---
+++ Assets/New Folder/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject target;
+    privet Vector3 offset;
+	// Use this for initialization
+	void Start () {
+     offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 delta = offset - (transform.position - target.transform.position);
+        transform.position += delta;
+	}
+}
