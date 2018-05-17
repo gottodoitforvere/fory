c9b93755cbc54dacc803f1d7f09f424f21fc0860
gottodoitforvere <38937564+gottodoitforvere@users.noreply.github.com>
2018-05-17 12:08:01 +0300

    Add files via upload

--- Assets/New Folder/playercountroler.cs
+++ Assets/New Folder/playercountroler.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class playercountroler : MonoBehaviour {
     public float speed;
     private Rigidbody rb;
+    private int score;
+    public Text scoreText;
+    private float jumpTime;
+   
     // Use this for initialization
     void Start() {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        scoreText.text = "Score: " + score.ToString();
+        jumpTime = 0;
     }
 
     // Update is called once per frame
@@ -15,17 +22,31 @@ public class playercountroler : MonoBehaviour {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         float j = Input.GetAxis("Jump");
+        if (j > 0)
+        {
+            jumpTime = jumpTime - Time.deltaTime;
+            if (jumpTime < 0) { j = 0; };
+        }
 
-        Vector3 dir = new Vector3(h, j * 4, v);
+        Vector3 dir = new Vector3(h, j * 5, v);
+
+       
 
         rb.AddForce(dir * speed);
     }
-     void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        { jumpTime = 1; }
+
+    }
+     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
-
+            score++;
+            scoreText.text = "Score: " + score.ToString();
         }
      }
     
